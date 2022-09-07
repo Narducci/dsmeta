e719181be57a9d13344e318811c59b06513a2f0e
Narducci <orazio.narducci@gmail.com>
2022-09-07 11:15:02 -0300

    Datepicker + correção CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <Header />
       <main>
         <section id="sales">
-          <div className="desmeta-container">
+          <div className="dsmeta-container">
             <SalesCard />
           </div>
         </section>
